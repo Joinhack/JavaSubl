36b9402ea876e6737d4ad3b5184350ad4be33612
joinhack <joinhack@gmail.com>
2015-08-01 00:34:58 +0800

    add

--- JavaGetterSetter.py
+++ JavaGetterSetter.py
@@ -75,8 +75,7 @@ class JavaGetterSetterCommand(sublime_plugin.TextCommand):
 
         try:
             properties_text = "\n" + "\n".join(getter_arr) + "\n" + "\n".join(setter_arr)
-            print(properties_text, insert_position)
-            insert_count = self.view.insert(insert_position, properties_text)
+            insert_count = self.view.insert(0, properties_text)
             self.view.sel().clear()
             self.view.sel().add(sublime.Region(insert_position, (insert_position + insert_count)))
         except Exception as ex:
